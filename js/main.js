49c79a7f0b8b1e2a9116029a4e766f4d516dbb05
bonniecampbell <bonnie.jp.campbell@hotmail.com>
2016-03-11 19:02:11 -0500

    switched the set up for my h1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-var $h1 = $('h1');
+var $h1 = $('<h1>');
+var $newLi = $('<li>');
 
-$h1.prepend(Below ground veggies);
+$h1.html('Below ground veggies');
+$('body').append($h1);
+
+$newLi.html();
+$('ul').prepend($newLi);
+
+$('ul li').each(function () {
+}
